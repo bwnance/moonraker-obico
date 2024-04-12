2296fab543e16330904f0699ab72f452fbbc31b4
Your Name <you@example.com>
2024-04-12 05:18:41 +0100

    Simplify the screen layout for debuggin

--- scripts/klipper_screen_obico_panel.py
+++ scripts/klipper_screen_obico_panel.py
@@ -34,22 +34,6 @@ class Panel(ScreenPanel):
         self.qr_image.set_vexpand(True)
         qr_code_container.pack_start(aspect_frame, True, True, 0)
 
-        self.action_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        padding = 20
-        self.action_container.set_margin_start(padding)
-        self.action_container.set_margin_end(padding)
-        self.action_container.set_margin_top(padding)
-        self.action_container.set_margin_bottom(padding)
-        self.top_box_container.pack_start(self.action_container, True, False, 0)
-
-        self.bottom_label = Gtk.Label()
-        self.bottom_label.set_hexpand(True)
-        self.bottom_label.set_halign(Gtk.Align.CENTER)
-        self.bottom_label.set_line_wrap(True)  # Enable line wrapping
-        self.bottom_label.set_line_wrap_mode(Pango.WrapMode.WORD)  # Break lines at word boundaries
-        self.bottom_label.set_margin_top(10)  # Add 10 pixels of margin on the bottom
-        self.main_container.pack_start(self.bottom_label, True, False, 0)
-
         self.content.show_all()
 
     def activate(self):
@@ -91,28 +75,15 @@ class Panel(ScreenPanel):
         self.reset_action_container()
         setup_label1 = self.create_label()
         setup_label1.set_markup(f"<big>Printer is linked to Obico server.</big>")
-        self.action_container.pack_start(setup_label1, True, False, 0)
 
         self.update_qr_code('https://obico.io/')
-
-        self.bottom_label.set_markup(f"<big>Scan the QR code to learn more about Obico.</big>")
 
     def display_link_qr_code(self, one_time_passcode, one_time_passlink):
         self.qr_code_label.set_markup(f"<big><b>Scan to Link Obico</b></big>")
 
         self.reset_action_container()
 
-        setup_label3 = self.create_label()
-        setup_label3.set_markup(f"<big>Or enter the code below in the Obico app:</big>")
-        self.action_container.pack_start(setup_label3, True, False, 0)
-        setup_label4 = self.create_label()
-        setup_label4.set_markup(f"<big><b>{one_time_passcode}</b></big>")
-        self.action_container.pack_start(setup_label4, True, False, 0)
-        self.action_container.show_all()
-
         self.update_qr_code(one_time_passlink)
-
-        self.default_bottom_text()
 
     def display_setup_guide_qr_code(self):
         self.qr_code_label.set_markup(f"<big><b>Scan to Set Up Obico</b></big>")
@@ -148,8 +119,7 @@ class Panel(ScreenPanel):
         self.qr_image.set_from_pixbuf(pixbuf)
 
     def reset_action_container(self):
-        for child in self.action_container.get_children():
-            self.action_container.remove(child)
+        pass
 
     def create_label(self):
         label = Gtk.Label()
@@ -158,8 +128,7 @@ class Panel(ScreenPanel):
         return label
 
     def default_bottom_text(self):
-        guide_text = "Obico is state-of-the-art AI and mobile app for 3D printing."
-        self.bottom_label.set_markup(f"<big>{guide_text}</big>")
+        pass
 
     def get_connected_moonraker_config(self, _screen):
         connected_printer_name = _screen.connected_printer
